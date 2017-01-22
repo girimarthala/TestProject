b0c3b97bc153ded7a55afb31d8408f3a936c8e42
girimarthala <giri.marthala@gmail.com>
2017-01-22 09:29:13 -0700

    Added 3rd print statement

--- src/test/Main.java
+++ src/test/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("This is a sample java class");
         System.out.println("This is a sample java class2");
 
+        System.out.println("This is a sample java class3");
     }
 }
